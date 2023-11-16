f022c22ece766245faf0387b8a59438291032049
ThuyDuong162 <117366206+ThuyDuong162@users.noreply.github.com>
2023-11-16 15:54:39 +0700

    tao tui code

--- FiveCafeApi-ejb/build/generated-sources/ap-source-output/com/fivecafe/entities/ImportDetailsPK_.java
+++ FiveCafeApi-ejb/build/generated-sources/ap-source-output/com/fivecafe/entities/ImportDetailsPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-16T11:42:40")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-16T15:50:11")
 @StaticMetamodel(ImportDetailsPK.class)
 public class ImportDetailsPK_ { 
 

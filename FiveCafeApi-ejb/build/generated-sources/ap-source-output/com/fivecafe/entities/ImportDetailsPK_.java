5eb4b8d660f93820cd2b8d59b10038c1cb839209
Son-KnownPain <sonhong443@gmail.com>
2023-11-23 16:27:38 +0700

    fix some bug

--- FiveCafeApi-ejb/build/generated-sources/ap-source-output/com/fivecafe/entities/ImportDetailsPK_.java
+++ FiveCafeApi-ejb/build/generated-sources/ap-source-output/com/fivecafe/entities/ImportDetailsPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-23T13:57:48")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-23T14:18:32")
 @StaticMetamodel(ImportDetailsPK.class)
 public class ImportDetailsPK_ { 
 
--- FiveCafeApi-ejb/build/generated-sources/ap-source-output/com/fivecafe/entities/Products_.java
+++ FiveCafeApi-ejb/build/generated-sources/ap-source-output/com/fivecafe/entities/Products_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-23T13:57:48")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-23T14:18:32")
 @StaticMetamodel(Products.class)
 public class Products_ { 
 

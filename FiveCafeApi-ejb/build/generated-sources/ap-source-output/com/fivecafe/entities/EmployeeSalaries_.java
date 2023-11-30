8a4b1909d3f4d2549365784621c493490cc43e6b
NhuDuongHuynh <124517406+NhuDuongHuynh@users.noreply.github.com>
2023-11-30 20:52:25 +0700

    push commit employeeSalary

--- FiveCafeApi-ejb/build/generated-sources/ap-source-output/com/fivecafe/entities/EmployeeSalaries_.java
+++ FiveCafeApi-ejb/build/generated-sources/ap-source-output/com/fivecafe/entities/EmployeeSalaries_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-28T20:17:57")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-11-30T20:48:17")
 @StaticMetamodel(EmployeeSalaries.class)
 public class EmployeeSalaries_ { 
 
---
+++ FiveCafeApi-war/src/java/com/fivecafe/body/employeesalary/CreateEmployeeSalaries.java
@@ -0,0 +1,24 @@
+package com.fivecafe.body.employeesalary;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateEmployeeSalaries {
+    @NotNull(message = "Employee ID is required")
+    private int employeeID;
+    
+    @NotEmpty(message = "Date is required")
+    @Pattern(regexp = "\\d{2}/\\d{2}/\\d{4}", message = "Invalid date format(dd/MM/yyyy), example: 01/10/2023")
+    private String date;
+}
